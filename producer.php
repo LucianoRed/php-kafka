9bbfb501f69eff932fe25176eaa0111f6e34b5b9
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2023-01-12 12:06:09 -0300

    Create producer.php

---
+++ producer.php
@@ -0,0 +1,22 @@
+<?php
+$brokers = getenv("KAFKA_BROKERS");
+$topic = getenv("KAFKA_TOPIC");
+
+$conf = new RdKafka\Conf();
+$conf->set('log_level', (string) LOG_DEBUG);
+$conf->set('debug', 'all');
+$rk = new RdKafka\Producer($conf);
+$rk->addBrokers("$brokers");
+$topic = $rk->newTopic("$topic");
+if($msglimit > 2000000) {
+	exit;
+}
+for($x=0;$x<$msglimit;$x++) {
+	$dttm = date();
+	$topic->produce(RD_KAFKA_PARTITION_UA, 0, "$dttm: Message payload $x");
+}
+$rk->flush($timeout_ms);
+$rk->purge(RD_KAFKA_PURGE_F_QUEUE);
+$rk->flush($timeout_ms);
+
+?>
