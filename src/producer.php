7f253add9ec449e44dbe66871b0cd2c22ae1318d
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2023-01-12 16:07:44 -0300

    Update producer.php

--- src/producer.php
+++ src/producer.php
@@ -2,39 +2,40 @@
 $brokers = getenv("KAFKA_BROKERS");
 $topic = getenv("KAFKA_TOPIC");
 if(isset($_GET['messages'])) {
-	$msglimit = intval($_GET['messages']);
+        $msglimit = intval($_GET['messages']);
 } else {
-	$msglimit = 100;	
+        $msglimit = 10;
+}
+if(isset($_GET['acks'])) {
+        $acks = intval($_GET['acks']);
+} else {
+        $acks = 0;
 }
 echo "Using kafka broker $brokers to send $msglimit on topic $topic<br>\n";
 
 $conf = new RdKafka\Conf();
+$conf->set('metadata.broker.list', $brokers);
+$conf->set('acks', $acks);
 $conf->set('log_level', (string) LOG_DEBUG);
 $conf->set('debug', 'all');
-$conf->set('acks', 0);
-$conf->set('bootstrap.servers', "$brokers");
-$rk = new RdKafka\Producer($conf);
-$rk->addBrokers("$brokers");
-$topic = $rk->newTopic("$topic");
-if($msglimit > 2000000) {
-	exit;
-}
-$count=0;
-$time_start = microtime(true);
-for($x=0;$x<$msglimit;$x++) {
-	$dttm = microtime(true);
-	$mensagem = "$dttm: Message payload $x";
-	echo "Produzindo mensagem $mensagem\n";
-	$topic->produce(RD_KAFKA_PARTITION_UA, 0, "$mensagem");
-	
-	//var_dump($topic);
-}
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-echo "Produced $msglimit messages in $time seconds!<br>\n";
-$rk->flush($timeout_ms);
-$rk->purge(RD_KAFKA_PURGE_F_QUEUE);
-$rk->flush($timeout_ms);
 
+//If you need to produce exactly once and want to keep the original produce order, uncomment the line below
+//$conf->set('enable.idempotence', 'true');
+
+$producer = new RdKafka\Producer($conf);
+
+$ctopic = $producer->newTopic("$topic");
+
+for ($i = 0; $i < $msglimit; $i++) {
+    $ctopic->produce(RD_KAFKA_PARTITION_UA, 0, "Message $i");
+            $producer->poll(0);
+            $result = $producer->flush(10000);
+            if (RD_KAFKA_RESP_ERR_NO_ERROR === $result) {
+                return true;
+            }
+        if (RD_KAFKA_RESP_ERR_NO_ERROR !== $result) {
+            throw new \RuntimeException('Was unable to flush, messages might be lost!');
+        }
+}
 
 ?>
