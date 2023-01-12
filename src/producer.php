c8d864579482fcd80dd98d708db41f87a1bd8de0
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2023-01-12 13:09:52 -0300

    Update producer.php

--- src/producer.php
+++ src/producer.php
@@ -16,12 +16,15 @@ if($msglimit > 2000000) {
 	exit;
 }
 $count=0;
+$time_start = microtime(true);
 for($x=0;$x<$msglimit;$x++) {
-	$dttm = date();
+	$dttm = microtime(true);
 	$topic->produce(RD_KAFKA_PARTITION_UA, 0, "$dttm: Message payload $x");
 	var_dump($topic);
 }
-echo "Produced $msglimit messages!";
+$time_end = microtime(true);
+$time = $time_end - $time_start;
+echo "Produced $msglimit messages in $time seconds!";
 $rk->flush($timeout_ms);
 $rk->purge(RD_KAFKA_PURGE_F_QUEUE);
 $rk->flush($timeout_ms);
